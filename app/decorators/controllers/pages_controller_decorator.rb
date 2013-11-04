7af32ce885f4592ac99132085e611fc38eafe28a
Anita Graham <anita@joli.com.au>
2013-11-04 13:23:36 +0800

    Remove debug messages, remove class="testimonials" from #testimonials

--- app/decorators/controllers/pages_controller_decorator.rb
+++ app/decorators/controllers/pages_controller_decorator.rb
@@ -1,17 +1,17 @@
 Refinery::PagesController.class_eval do
   append_before_filter :get_testimonials, :only => [:show]
   puts "Appended before filter get_testimonials 2"
-  
+
   protected
-   
+
     def get_testimonials
-      logger.debug "---------- Checking Testimonials---------------"     
-      if @page.testimonials_show 
+      # logger.debug "---------- Checking Testimonials---------------"
+      if @page.testimonials_show
         @testimonials = Refinery::Testimonials::Testimonial.scoped
-        logger.debug "---------- There are  #{@testimonials.count} in total ---------------"
+        # logger.debug "---------- There are  #{@testimonials.count} in total ---------------"
         @testimonials = page.testimonials_select=='Random' ? @testimonials.sample : @testimonials.recent
         @testimonials = @testimonials.limit(page.testimonials_count  ) unless page.testimonials_count==0
-        logger.debug "---------- Making #{@testimonials.count} available---------------"
+        # logger.debug "---------- Making #{@testimonials.count} available---------------"
       end
     end
 end
