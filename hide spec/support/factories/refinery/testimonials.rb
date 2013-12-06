a8cae89aa0ca21b730431382f4f560443aba95a2
Anita Graham <anita@joli.com.au>
2013-12-06 15:31:44 +0800

    delete orphan files

--- hide spec/support/factories/refinery/testimonials.rb
+++
@@ -1,7 +0,0 @@
-
-FactoryGirl.define do
-  factory :testimonial, :class => Refinery::Testimonials::Testimonial do
-    sequence(:name) { |n| "refinery#{n}" }
-  end
-end
-
--- hide spec/test/unit/page_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PageTest < ActiveSupport::TestCase
-
-  context "The Page class has been extended and now it" do
-    should_have_instance_methods :testimonials_show, :testimonials_count, :testimonials_select
-  end
-
-end
